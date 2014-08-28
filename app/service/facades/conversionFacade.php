4e8bf12bb5de6e428d84f58ee4a076de91ba7ea0
Chris Goosey <chris@dfg.gd>
2014-08-28 14:02:00 +0100

    Added track naming when chosing the track

---
+++ app/service/facades/conversionFacade.php
@@ -0,0 +1,9 @@
+<?php namespace Service;
+
+use \Illuminate\Support\Facades\Facade;
+
+class ConversionFacade extends Facade {
+
+    protected static function getFacadeAccessor() { return 'Service\ConversionService'; }
+
+}
